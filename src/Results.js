93e1abb1fb03066e612744fe883459169deb80a0
santdas36 <santdas36@gmail.com>
2020-08-30 23:03:57 +0530

    rs style

--- src/Results.js
+++ src/Results.js
@@ -13,34 +13,31 @@ function Results({ searchResult, setMovieId }) {
 	}
 
 	return (
-		<div className="list results">
-
-			<div class="list__trending list__big">
-				<h4>Search Results</h4>
-				<div class="list__items list__items-big">
-					{ searchResult?.map((movie) =>
-						(<div class="list__item" onClick={() => handleClick(movie)}>
-							<img src={`${imageBase}${movie.poster_path || movie.backdrop_path}`} />
-							<div className="list__itemInfo">
-								<h5 className="list__itemTitle">{movie.title || movie.original_title || movie.name || movie.original_name}</h5>
-								<TextTruncate
-									line={2}
-									element="p"
-									containerClassName="list__itemOverview"
-									truncateText="…"
-									text={movie.overview}
-								/>
-								{ movie.vote_average && 
-								<div className="list__rating">
-									<Rating name="movie-rating" className="movieRating" value={movie.vote_average / 2} precision={0.5} icon={<StarRoundedIcon fontSize="inherit" readOnly />}/>
-									<small className="list__likes">{movie.vote_average / 2}</small>
-								</div> }
-							</div>
-						</div>)
-					)}
-				</div>
+		<div className="results">
+			<h4>Search Results</h4>
+			<div class="results__list">
+				{ searchResult?.map((movie) =>
+					(<div class="list__item" onClick={() => handleClick(movie)}>
+						<img src={`${imageBase}${movie.poster_path || movie.backdrop_path}`} />
+						<h5 className="list__itemType">{movie.media_type}<h5>
+						<div className="list__itemInfo">
+							<h5 className="list__itemTitle">{movie.title || movie.original_title || movie.name || movie.original_name}</h5>
+							<TextTruncate
+								line={2}
+								element="p"
+								containerClassName="list__itemOverview"
+								truncateText="…"
+								text={movie.overview}
+							/>
+							{ movie.vote_average && 
+							<div className="list__rating">
+								<Rating name="movie-rating" className="movieRating" value={movie.vote_average / 2} precision={0.5} icon={<StarRoundedIcon fontSize="inherit" readOnly />}/>
+								<small className="list__likes">{movie.vote_average / 2}</small>
+							</div> }
+						</div>
+					</div>)
+				)}
 			</div>
-
 		</div>
 	);
 }
